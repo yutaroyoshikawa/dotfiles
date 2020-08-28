2b2281157b7ce67b2a0cbade23ed995738bd986f
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-08-28 17:41:26 +0900

    add hyper dotfile

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zprezto
+ln -sf ~/dotfiles/.hyper.js ~/.hyper.js
+
