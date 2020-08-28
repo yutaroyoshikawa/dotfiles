2f806241cdad138a61008a285235c90daf985930
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-08-28 22:23:22 +0900

    add alias

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,4 +5,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc_Linux
 ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zprezto
 ln -sf ~/dotfiles/.hyper.js ~/.hyper.js
+ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
 
